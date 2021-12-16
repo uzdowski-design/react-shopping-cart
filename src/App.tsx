b66ffd7c2603afc312e5db4ae888b36689b07669
Michal Uzdowski <uzdowski.design@gmail.com>
2021-12-16 01:06:30 +0100

    created products grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { useQuery } from 'react-query';
 
-function App() {
+// Components
+import Item from './components/Item/Item';
+// import Drawer from '@material-ui/core/Drawer';
+// import LinearProgress from '@material-ui/core/LinearProgress';
+// import Grid from '@material-ui/core/Grid';
+import { Drawer, LinearProgress, Grid, Badge } from '@material-ui/core';
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
+
+// Styles
+import { Wrapper } from './App.styles';
+
+// Types
+export type CartItemType = {
+  id: number,
+  category: string,
+  description: string,
+  image: string,
+  price: number,
+  title: string,
+  amount: number
+}
+
+// get products from API
+const getProducts = async (): Promise<CartItemType[]> =>
+  await (await fetch('https://fakestoreapi.com/products')).json()
+
+
+const App = () => {
+  const [cartOpen, setCartOpen] = useState(false);
+  const [cartItems, setCartItems] = useState([] as CartItemType[])
+  const { data, isLoading, error } = useQuery<CartItemType[]>('products', getProducts)
+  console.log(data)
+
+  const getTotalItems = () => null;
+
+  const handleAddToCart = (clickedItem: CartItemType) => null;
+
+  const handleRemoveFromCart = () => null;
+
+  if (isLoading) return <LinearProgress />
+  // if (isLoading) return <CircularProgress />
+  if (error) return <div>Something went wrong...</div>
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Drawer anchor='right' open={cartOpen} onClose={() => setCartOpen(false)}>
+        Cart
+      </Drawer>
+      <Grid container spacing={3}>
+        {data?.map(item => (
+          <Grid item key={item.id} xs={12} sm={4}>
+            <Item item={item} handleAddToCart={handleAddToCart} />
+          </Grid>
+        ))}
+      </Grid>
+    </Wrapper>
   );
 }
 
